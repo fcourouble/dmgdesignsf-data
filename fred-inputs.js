3f27bf04544d660db5a45bcb685941a1a74a8760
Armelle Laine <laine@aptolsystems.com>
2020-04-11 16:55:01 -0700

    fred

---
+++ fred-inputs.js
@@ -0,0 +1,28 @@
+const services = [
+  {
+    category: 'aerospace',
+    service: 'skill1',
+    title: 'title',
+    details: 'details',
+    images: ['filename1', 'filename2']
+  }
+]
+const projects = [
+  {
+    project: 'project name',
+    title: 'title',
+    details: 'details',
+    year: 'year',
+    location: 'location'
+  }
+]
+const images_entry = [
+  {
+    name: 'filename',
+    cat: 'aerospace',
+    project: 'gf42',
+    tag: 'concept',
+    title: 'what',
+    caption: 'details' // null
+  }
+]
